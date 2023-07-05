4acdbbed5a1c603ef6abd5058f4ccc772ceafe39
Liam Wilson <cosinusoidally@gmail.com>
2023-07-05 01:19:20 +0100

    fix check_syms.sh

--- check_syms.sh
+++ check_syms.sh
@@ -12,3 +12,7 @@ for i in *.o;do
   echo $i
   readelf -a $i|grep UND|grep __
 done
+
+# supress non-zero exit codes since there will be a non-zero exit code if there
+# are no undefined symbols (which is expected).
+exit 0
