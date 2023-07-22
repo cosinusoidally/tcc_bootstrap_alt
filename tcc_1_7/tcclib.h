d23abb969f69a6aebd0a67f04947f04bad2e8a8b
Liam Wilson <cosinusoidally@gmail.com>
2023-07-22 13:04:16 +0100

    fix gcc build for 1_7

--- tcc_1_7/tcclib.h
+++ tcc_1_7/tcclib.h
@@ -68,3 +68,9 @@ void *dlopen(const char *filename, int flag);
 const char *dlerror(void);
 void *dlsym(void *handle, char *symbol);
 int dlclose(void *handle);
+
+#define PROT_READ      0x1
+#define PROT_WRITE     0x2
+#define PROT_EXEC      0x4
+#define MAP_PRIVATE    0x02
+#define MAP_ANONYMOUS  0x20
