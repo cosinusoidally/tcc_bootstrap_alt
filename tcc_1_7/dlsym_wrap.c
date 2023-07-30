8922104d5aeac7edf335d8a454cf4e44c142b6d1
Liam Wilson <cosinusoidally@gmail.com>
2023-07-30 14:53:04 +0100

    start adding code to allow wrapping of libc functions

--- tcc_1_7/dlsym_wrap.c
+++ tcc_1_7/dlsym_wrap.c
@@ -7,14 +7,34 @@ void *dlsym_wrap2(void *handle, char *symbol)
 
 #include "tcc.c"
 
+typedef struct TCCSyms {
+    char *str;
+    void *ptr;
+} TCCSyms;
+
+static TCCSyms tcc_syms[] = {
+    { NULL, NULL },
+};
+
 void *dlsym_wrap(void *handle, char *symbol)
 {
 //if (!strcmp(p->str, symbol))
+  TCCSyms *s;
+  s=tcc_syms;
+
   int p;
+  int i=0;
   printf("dlsym: %s\n",symbol);
   if(!strcmp("dlsym", symbol)) {
     return &dlsym_wrap;
   }
+
+  while(s->str != NULL) {
+    if (!strcmp(s->str, symbol))
+      return s->ptr;
+    s++;
+  }
+
   p=dlsym_wrap2(handle,symbol);
   return p;
 }
