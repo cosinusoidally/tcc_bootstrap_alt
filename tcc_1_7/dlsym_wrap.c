f6fce2725e446303ce092995b4e884f0aa3593bc
Liam Wilson <cosinusoidally@gmail.com>
2023-07-30 14:43:01 +0100

    double wrap to allow me to incrementally override stuff

--- tcc_1_7/dlsym_wrap.c
+++ tcc_1_7/dlsym_wrap.c
@@ -1,3 +1,12 @@
+void *dlsym_wrap2(void *handle, char *symbol)
+{
+  return dlsym(handle,symbol);
+}
+
+#define dlsym dlsym_wrap
+
+#include "tcc.c"
+
 void *dlsym_wrap(void *handle, char *symbol)
 {
 //if (!strcmp(p->str, symbol))
@@ -6,10 +15,6 @@ void *dlsym_wrap(void *handle, char *symbol)
   if(!strcmp("dlsym", symbol)) {
     return &dlsym_wrap;
   }
-  p=dlsym(handle,symbol);
+  p=dlsym_wrap2(handle,symbol);
   return p;
 }
-
-#define dlsym dlsym_wrap
-
-#include "tcc.c"
