73369a066b7ff294fb5c0b99b0d8fd9ca80ab0ad
Liam Wilson <cosinusoidally@gmail.com>
2023-07-30 14:34:00 +0100

    use dlsym_wrap for subinvocations of tcc

--- tcc_1_7/dlsym_wrap.c
+++ tcc_1_7/dlsym_wrap.c
@@ -3,6 +3,9 @@ void *dlsym_wrap(void *handle, char *symbol)
 //if (!strcmp(p->str, symbol))
   int p;
   printf("dlsym: %s\n",symbol);
+  if(!strcmp("dlsym", symbol)) {
+    return &dlsym_wrap;
+  }
   p=dlsym(handle,symbol);
   return p;
 }
