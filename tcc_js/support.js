ab55d106bcc9a2e859a6d41974a5b61211cb523b
Liam Wilson <cosinusoidally@gmail.com>
2023-08-22 20:16:55 +0100

    more

--- tcc_js/support.js
+++ tcc_js/support.js
@@ -1,1 +1,28 @@
 print("loading support code");
+
+var heap_size=16*1024*1024;
+var stack_size=256*1024;
+var heap=new Array(heap_size/4);
+for(var i=0;i<heap_size/4;i++){
+  heap[i]=0;
+};
+
+var esp=heap_size-4;
+var ebp=esp;
+
+function to_hex(x){
+  var y;
+  var a=[];
+  while(x){
+    y=x&0xff;
+    x=x>>>8;
+    a.push(y);
+  }
+  var b=[];
+  while(a.length>0){
+    b.push(a.pop());
+  }
+  return "0x"+("00000000"+(b.map(function(z){return ("0000"+z.toString(16)).slice(-2)}).join(""))).slice(-8);
+}
+print(to_hex(heap_size));
+print(to_hex(esp));
