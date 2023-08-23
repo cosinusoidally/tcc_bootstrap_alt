3f1c84d55a3607db845db6bbeaed0efc333710cd
Liam Wilson <cosinusoidally@gmail.com>
2023-08-23 20:55:57 +0100

    more

--- tcc_js/gen_metadata.c
+++ tcc_js/gen_metadata.c
@@ -6,7 +6,7 @@
 
 int main(void){
   printf("// use gen_metadata.c to create\n");
-  printf("var TOK_IDENT=0x%x\n",TOK_IDENT);
-  printf("var SYM_FIRST_ANOM=0x%x\n",SYM_FIRST_ANOM);
+  printf("var TOK_IDENT=0x%x;\n",TOK_IDENT);
+  printf("var SYM_FIRST_ANOM=0x%x;\n",SYM_FIRST_ANOM);
   return 0;
 }
