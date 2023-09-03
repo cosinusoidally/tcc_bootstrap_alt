e0d5195c407929eab8d0c58ca0b281557745152f
Liam Wilson <cosinusoidally@gmail.com>
2023-09-03 21:44:44 +0100

    more

--- tcc_js/test3.js
+++ tcc_js/test3.js
@@ -1,7 +1,7 @@
 load("tcc.js");
 
 // dummy main call:
-argc_argv=mk_argc_argv("tcc -r test2.c");
+argc_argv=mk_argc_argv("tcc -r test3.c");
 print(JSON.stringify(argc_argv));
 //err();
 main(argc_argv.argc,argc_argv.argv);
