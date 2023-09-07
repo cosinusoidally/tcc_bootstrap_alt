2b43476e93935264634370bb54a375155baa136b
Liam Wilson <cosinusoidally@gmail.com>
2023-09-07 17:36:50 +0100

    update test6.c test

--- tcc_js/test6.c
+++ tcc_js/test6.c
@@ -1,8 +1,13 @@
 int z=100;
 
+int bar(int x){
+  return x+2;
+}
+
 int main(void){
   int r=2;
   int foo=3;
+  printf("bar(7): %d\n",bar(7));
   printf("z: %d\n",z);
   puts("hello world");
   return r+foo;
