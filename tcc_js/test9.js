a8d0a9d5871292e1793ea781c084c39c4d3a74f9
Liam Wilson <cosinusoidally@gmail.com>
2023-09-08 18:07:47 +0100

    more

--- tcc_js/test9.js
+++ tcc_js/test9.js
@@ -6,4 +6,4 @@ print(JSON.stringify(argc_argv));
 //err();
 main(argc_argv.argc,argc_argv.argv);
 
-check("e05897351ce5d40fb4687a0e87cd33f0f94e8523f1edd8a5b4a419cf9c55efa7");
+check("24b3577a484610dac70219d108d3ec1007704ca6a31a8b6202bcb1241d0a201f");
