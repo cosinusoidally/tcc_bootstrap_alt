9df9289ce09721a2d0c3b114060362e417cefd2e
Liam Wilson <cosinusoidally@gmail.com>
2023-09-12 18:38:32 +0100

    fix hash

--- tcc_js/test-tcc.js
+++ tcc_js/test-tcc.js
@@ -8,4 +8,4 @@ print(JSON.stringify(argc_argv));
 //err();
 main(argc_argv.argc,argc_argv.argv);
 
-check("0b99729875157cf0007b39b70bfb816f4a59ad4849f6c87d7706d2fa35cabef8",true);
+check("441f972443c4c77d66d39ecfdf0f87be7987a663325a41777472bf6b35383afd",true);
