01e9536db80b1bdd0685f3725a35a70748033d26
Liam Wilson <cosinusoidally@gmail.com>
2023-10-03 20:16:07 +0100

    more

--- tcc_js/js_to_c.js
+++ tcc_js/js_to_c.js
@@ -1,19 +1,24 @@
 f=read("tcc.js").split("\n").reverse();
 g=[];
+
+b=[];
+
 while(f.length>0){
   l=f.pop();
   t=l.slice(0,3);
   if(t==="// ") {
     continue;
   }
+  t=l.slice(0,4);
   if(t==="var "){
     g.push(l);
     continue;
   }
-  
-  print(l);
+  b.push(l);
 }
 
-for(var i=0;i<g.length;g++){
-  print(g[i]);
+for(var i=0;i<g.length;i++){
+  c=g[i];
+  print(c);
+  gd=[];
 }
