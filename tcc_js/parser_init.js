c562b96c9260866f834ad3c02d998e4cd84534f1
Liam Wilson <cosinusoidally@gmail.com>
2023-10-04 23:39:02 +0100

    more

--- tcc_js/parser_init.js
+++ tcc_js/parser_init.js
@@ -18,8 +18,49 @@ function get_twochar_str(){
     t=malloc(256);
     print("hex dump 1");hd(twochar_str,256);
     var o=t;
+//  <=\236
     o=wsb(o,mk_c_string("<="),0236);
-    o=wsb(o,mk_c_string(">="),0236);
+//  >=\235
+    o=wsb(o,mk_c_string(">="),0235);
+//  !=\225
+    o=wsb(o,mk_c_string("!="),0225);
+//  &&\240
+    o=wsb(o,mk_c_string("&&"),0240);
+//  ||\241
+    o=wsb(o,mk_c_string("||"),0241);
+//  ++\244
+    o=wsb(o,mk_c_string("++"),0244);
+//  --\242
+    o=wsb(o,mk_c_string("--"),0242);
+//  ==\224
+    o=wsb(o,mk_c_string("=="),0224);
+//  <<\1
+    o=wsb(o,mk_c_string("<<"),1);
+//  >>\2
+    o=wsb(o,mk_c_string(">>"),2);
+//  +=\253
+    o=wsb(o,mk_c_string("+="),0253);
+//  -=\255
+    o=wsb(o,mk_c_string("-="),0255);
+//  *=\252
+    o=wsb(o,mk_c_string("*="),0252);
+//  /=\257
+    o=wsb(o,mk_c_string("/="),0257);
+//  %=\245
+    o=wsb(o,mk_c_string("%="),0245);
+//  &=\246
+    o=wsb(o,mk_c_string("&="),0246);
+//  ^=\336
+    o=wsb(o,mk_c_string("^="),0336);
+//  |=\374
+    o=wsb(o,mk_c_string("|="),0374);
+//  ->\247
+    o=wsb(o,mk_c_string("->"),0247);
+//  ..\250
+    o=wsb(o,mk_c_string(".."),0250);
+//  ##\266
+    o=wsb(o,mk_c_string("##"),0266);
     print("hex dump 2");hd(t,256);
+    o=wsb(o,mk_c_string("<="),0236);
     return twochar_str;
 }
