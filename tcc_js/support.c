f21e7c235156464d881be42562640010a2e549a5
Liam Wilson <cosinusoidally@gmail.com>
2023-10-05 01:44:16 +0100

    more

--- tcc_js/support.c
+++ tcc_js/support.c
@@ -33,56 +33,80 @@ int urs(int a1,int a2) {
   err();
 }
 
-int error(int a1) {
-  puts("unimpl err");
-  err();
-}
-int err(void) {
-  puts("err");
-  exit(1);
-}
-void v_free(int x) {
-  puts("unimpl urs");
-  err();
-}
+/* virtual heap memory functions */
+
 int v_alloca(int x) {
   puts("unimpl alloca");
   err();
 }
-int mk_c_string(char *s){
-  puts("unimpl mk_c_string");
-}
-int v_strcpy(int a1, int a2) {
-  puts("unimpl strcpy");
-  err();
-}
-int v_strlen(int len) {
-  puts("unimpl strlen");
-  err();
-}
-int mk_char(int a) {
-  puts("unimpl mk_char");
-  err();
-}
+
 int enter(void) {
   puts("unimpl enter");
-  err();
-}
-int memcmp(int a, int b, int c) {
-  puts("unimpl memcmp");
   err();
 }
 int leave(int a) {
   puts("unimpl leave");
   err();
 }
+
 int v_realloc(int a,int b) {
   puts("unimpl realloc");
   err();
 }
+
+void v_free(int x) {
+  puts("unimpl urs");
+  err();
+}
+
+int v_memcmp(int a, int b, int c) {
+  puts("unimpl memcmp");
+  err();
+}
+
 int v_memcpy(int a, int b, int c) {
   puts("unimpl memcpy");
   err();
+}
+/* virtual heap string functions */
+
+int mk_char(int a) {
+  puts("unimpl mk_char");
+  err();
+}
+
+int mk_c_string(char *s){
+  puts("unimpl mk_c_string");
+}
+
+int v_strcat(int a,int b) {
+  puts("unimpl strcat");
+  err();
+}
+int v_strcpy(int a1, int a2) {
+  puts("unimpl strcpy");
+  err();
+}
+
+int v_strdup(int a) {
+  puts("unimpl strdup");
+  err();
+}
+
+int v_strlen(int len) {
+  puts("unimpl strlen");
+  err();
+}
+
+int v_strrchr(int a, int b) {
+  puts("unimpl urs");
+  err();
+}
+
+/* error functions */
+int err(void) {
+  puts("err");
+  exit(1);
 }
 int fclose(int a) {
   puts("unimpl fclose");
@@ -92,20 +116,8 @@ int v_getc_unlocked(int a) {
   puts("unimpl getc_unlocked");
   err();
 }
-int v_strrchr(int a, int b) {
-  puts("unimpl urs");
-  err();
-}
-int v_strcat(int a,int b) {
-  puts("unimpl strcat");
-  err();
-}
 int v_fopen(int a, int b) {
   puts("unimpl fopen");
-  err();
-}
-int v_strdup(int a) {
-  puts("unimpl strdup");
   err();
 }
 int v_fwrite(int a,int b, int c, int d) {
