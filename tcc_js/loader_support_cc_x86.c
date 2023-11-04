db245d59506399d21cdce1c359e37a19547a5469
Liam Wilson <cosinusoidally@gmail.com>
2023-11-04 22:28:00 +0000

    more

--- tcc_js/loader_support_cc_x86.c
+++ tcc_js/loader_support_cc_x86.c
@@ -16,9 +16,32 @@ int puts(char *a) {
   fputs("\n",stdout);
 }
 
+void puts_num(int x){
+  char *s;
+  s=int2str(x,10,0);
+  puts(s);
+  free(s);
+  return;
+}
+
 int fread(int ptr,int size, int nitems, int stream) {
+/*
   puts("fread not impl");
-/*  exit(1); */
+  exit(1);
+*/
+  puts("fread");
+  int t=size*nitems;
+  puts("initial t:");
+puts_num(t);
+  char *c=ptr;
+  int c;
+  while(t>0){
+puts_num(t);
+    c[0]=fgetc(stream);
+puts_num(c[0]);
+    t=t-1;
+    c=c+1;
+  }
 }
 
 int memcpy(int a, int b, int c) {
