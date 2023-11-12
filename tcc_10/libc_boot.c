16d1eceddc553d90baad24040526925f8ad2f2de
Liam Wilson <cosinusoidally@gmail.com>
2023-11-12 20:12:00 +0000

    more stubs

--- tcc_10/libc_boot.c
+++ tcc_10/libc_boot.c
@@ -30,6 +30,106 @@ int strcmp(void){
   exit(1);
 }
 
+int strcat(void){
+  puts("strcat not impl");
+  exit(1);
+}
+
+int fprintf(void){
+  puts("fprintf not impl");
+  exit(1);
+}
+
+int vfprintf(void){
+  puts("vfprintf not impl");
+  exit(1);
+}
+
+int memcmp(void){
+  puts("memcmp not impl");
+  exit(1);
+}
+
+int memcpy(void){
+  puts("memcpy not impl");
+  exit(1);
+}
+
+int sprintf(void){
+  puts("sprintf not impl");
+  exit(1);
+}
+
+int read(void){
+  puts("read not impl");
+  exit(1);
+}
+
+int memmove(void){
+  puts("memmove not impl");
+  exit(1);
+}
+
+int strrchr(void){
+  puts("strrchr not impl");
+  exit(1);
+}
+
+int ldexp(void){
+  puts("ldexp not impl");
+  exit(1);
+}
+
+int snprintf(void){
+  puts("snprintf not impl");
+  exit(1);
+}
+
+int getcwd(void){
+  puts("getcwd not impl");
+  exit(1);
+}
+
+int dlsym(void){
+  puts("dlsym not impl");
+  exit(1);
+}
+
+int fwrite(void){
+  puts("fwrite not impl");
+  exit(1);
+}
+
+int lseek(void){
+  puts("lseek not impl");
+  exit(1);
+}
+
+int strtol(void){
+  puts("strtol not impl");
+  exit(1);
+}
+
+int strchr(void){
+  puts("strchr not impl");
+  exit(1);
+}
+
+int sigemptyset(void){
+  puts("sigemptyset not impl");
+  exit(1);
+}
+
+int sigaction(void){
+  puts("sigaction not impl");
+  exit(1);
+}
+
+int dlopen(void){
+  puts("dlopen not impl");
+  exit(1);
+}
+
 int printf(int x){
   puts("printf not impl");
   exit(1);
