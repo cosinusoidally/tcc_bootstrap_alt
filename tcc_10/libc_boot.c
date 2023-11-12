deb87e503a68bd65f58ae5a8352b792d9d4c7e62
Liam Wilson <cosinusoidally@gmail.com>
2023-11-12 19:52:47 +0000

    more

--- tcc_10/libc_boot.c
+++ tcc_10/libc_boot.c
@@ -1,4 +1,16 @@
+extern int stdout;
+
+int puts(int x) {
+  fputs(x, stdout);
+  fputs("\n", stdout);
+}
+
 int printf(int x){
   puts("printf not impl");
+  exit(1);
+}
+
+int malloc(int x){
+  puts("malloc not impl");
   exit(1);
 }
