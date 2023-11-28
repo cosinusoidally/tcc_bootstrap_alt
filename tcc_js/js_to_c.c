5e103052d6d8949ac0b4b0788ca65d2acaa01242
Liam Wilson <cosinusoidally@gmail.com>
2023-11-28 19:52:41 +0000

    more

--- tcc_js/js_to_c.c
+++ tcc_js/js_to_c.c
@@ -1,20 +1,47 @@
 #include "elf_loader_support_tcc.c"
 
+int l_size;
+
+int wu8(int o, int v) {
+  char *b;
+  b=o;
+  b[0] = v & 0xFF;
+}
+
+int init_globals(void){
+  l_size=256;
+}
+
+int process_line(int l) {
+  puts("process line");
+}
+
 int process_file(int name){
   int f;
   int c;
   int lb;
-  lb=malloc(256);
+  int lo;
+  lb=malloc(l_size);
   f=fopen(name,"rb");
+  lo=0;
   while((c=fgetc(f)) != (-1)) {
     fputc(c,stdout);
-  } 
+    wu8(lb+lo,c);
+    if(c=='\n'){
+      wu8(lb+lo,0);
+      lo=0;
+      process_line(lb);
+    } else {
+      lo=lo+1;
+    }
+  }
   free(lb); 
   return 0;
 }
 
 int main(void){
   init_c();
+  init_globals();
   process_file("tcc.js");
   return 0;
 }
