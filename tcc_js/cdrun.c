47244071f61eda83379880edd45c88e0a74c3598
Liam Wilson <cosinusoidally@gmail.com>
2023-12-01 06:17:18 +0000

    more cdrun.c

--- tcc_js/cdrun.c
+++ tcc_js/cdrun.c
@@ -1,3 +1,19 @@
 int main(int argc, int **argv){
-  return 0;
+  int argc_e;
+  int **argv_e;
+  int name;
+  argc_e=argc-2;
+  argv_e=argv;
+  argv_e=argv_e+2;
+  name=argv_e[0];
+  puts("cdrun");
+  puts(name);
+  puts(argv_e[1]);
+  chdir(argv[1]);
+  if(argv_e[argc_e]!=0){
+    puts("last argv is not null");
+    exit(1);
+  }
+
+  return execve(name,argv_e,0);
 }
