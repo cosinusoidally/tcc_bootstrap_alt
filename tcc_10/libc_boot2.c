9125e687c8fb521ed526efdc7cab0a075379fae5
Liam Wilson <cosinusoidally@gmail.com>
2023-12-03 02:10:19 +0000

    debug logging for strtod

--- tcc_10/libc_boot2.c
+++ tcc_10/libc_boot2.c
@@ -68,7 +68,9 @@ double strtod(char *a, char **p){
   x=&v;
   x[0]=0;
   x[1]=0;
-  puts("tcc strtod");
+  fputs("tcc strtod: ",stdout);
+  fputs(a,stdout);
+  fputs("\n",stdout);
   if(strcmp("4294967296.0",a) == 0){
     x[1]=0x41F00000;
   }
