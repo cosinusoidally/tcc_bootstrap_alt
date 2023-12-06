16ed26f6286ac145bbbe2fab21e8aefa0213629e
Liam Wilson <cosinusoidally@gmail.com>
2023-12-06 17:00:38 +0000

    more

--- tcc_27/run_27_more.sh
+++ tcc_27/run_27_more.sh
@@ -1,6 +1,16 @@
-# run tcc_27
-../x86/bin/elf_loader_cc_x86.exe -l libtcc1.o -l ../tcc_26/libc_boot3.o -l tcc.o -c ./lib/libtcc1.c -o libtcc1_new.o
-../x86/bin/elf_loader_cc_x86.exe -l libtcc1.o -l ../tcc_26/libc_boot3.o -l tcc.o -I ../woody/usr/include/ -I include -c tcc.c -DONE_SOURCE -o tcc_new.o
+# build libc_min.o
 
-../x86/bin/catm tcc.o tcc_new.o
-../x86/bin/catm libtcc1.o libtcc1_new.o
+../x86/bin/elf_loader_cc_x86.exe -l ../tcc_10/libc_boot.o -l ../tcc_10/tcc.o -o libc_min.o -c ../tcc_10/libc_min.c
+
+# make sure ..new.o files are empty
+
+../x86/bin/catm tcc_new.o
+../x86/bin/catm libtcc1_new.o
+
+# run tcc_27
+../x86/bin/elf_loader_cc_x86.exe -l libtcc1.o -l libc_min.o -l tcc.o -c ./lib/libtcc1.c -o libtcc1_new.o
+../x86/bin/elf_loader_cc_x86.exe -l libtcc1.o -l libc_min.o -l tcc.o -I ../woody/usr/include/ -I include -c tcc.c -DONE_SOURCE -o tcc_new.o
+
+sha256sum *o
+diff -u -s libtcc1_new.o libtcc1.o
+diff -u -s tcc_new.o tcc.o
