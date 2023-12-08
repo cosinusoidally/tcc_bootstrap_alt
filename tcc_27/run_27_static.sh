e1ce676d816f0be400fda0d1538d23152bbb30c0
Liam Wilson <cosinusoidally@gmail.com>
2023-12-08 19:07:18 +0000

    more

--- tcc_27/run_27_static.sh
+++ tcc_27/run_27_static.sh
@@ -4,6 +4,8 @@ rm libc_boot4*.o tcc_new.o libtcc1_new.o libc_static.o ../artifacts/tcc-27-stati
 
 chmod +x ../artifacts/tcc_27_boot_static.exe
 
+../artifacts/tcc_27_boot_static.exe -I ../woody/usr/include/ -I include -c tcc.c -DONE_SOURCE -o tcc_new2.o
+
 sha256sum *o
 ls -l *o
 ls -l ../artifacts/tcc_27_boot_static.exe
