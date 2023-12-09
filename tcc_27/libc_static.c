b29f6b3b8171f56488747680da37d930f0b1a8be
Liam Wilson <cosinusoidally@gmail.com>
2023-12-09 11:56:27 +0000

    license stuff

--- tcc_27/libc_static.c
+++ tcc_27/libc_static.c
@@ -1,3 +1,9 @@
+/*
+ * Cobbled together from parts of M2libc and GNU Mes libc
+ * Changes are (C) 2023 Liam Wilson and collectively Licensed under GPLv3
+ * as defined below
+ */
+
 /* -*-comment-start: "//";comment-end:""-*-
  * GNU Mes --- Maxwell Equations of Software
  * Copyright © 2017,2018,2019,2023 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
@@ -17,6 +23,23 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with GNU Mes.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Copyright (C) 2016 Jeremiah Orians
+ * This file is part of M2-Planet.
+ *
+ * M2-Planet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * M2-Planet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with M2-Planet.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 int stdin=0;
