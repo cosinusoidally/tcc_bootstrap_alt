740d677fc2d9a3b1644d87e45adf1947cb409c33
Liam Wilson <cosinusoidally@gmail.com>
2024-03-17 23:27:09 +0000

    more

--- otccelf/otccelf_full.c
+++ otccelf/otccelf_full.c
@@ -27,76 +27,76 @@ an (){
 }
 
 av(){
-int a,s,h;
-while( isspace(m)|m == 35){
-if( m == 35){
-w ();
-av();
-if( e == 536){
-av();
-L(32);
-*(int*) e=1;
-*(int*)(e+4)=D;
-}
-while( m!=10){
-L(m);
-w ();
-}
-L(m);
-L(2);
-}
-w ();
-}
-J=0;
-e=m;
-if( am ()){
-L(32);
-Z=D;
-while( am ()){
-L(m);
-w ();
-}
-if( isdigit(e)){
-C=strtol(Z,0,0);
-e=2;
-}
-else{
-*(char*) D=32;
-e=strstr(Q,Z-1)-Q;
-*(char*) D=0;
-e=e*8+256;
-if( e>536){
-e=T+e;
-if( *(int*) e == 1){
-V=*(int*)(e+4);
-al=m;
-w ();
-av();
-}
-}
-}
-}
-else{
-w ();
-if( e == 39){
-e=2;
-an ();
-C=m;
-w ();
-w ();
-}
-else if( e == 47&m == 42){
-w ();
-while( m){
-while( m!=42)w ();
-w ();
-if( m == 47)m=0;
-}
-w ();
-av();
-}
-else{
-a="++#m--%am*@R<^1c/@%[_[H3c%@%[_[H3c+@.B#d-@%:_^BKd<<Z/03e>>`/03e<=0f>=/f<@.f>@1f==&g!='g&&k||#l&@.BCh^@.BSi|@.B+j~@/%Yd!@&d*@b";
+  int a,s,h;
+  while( isspace(m)|m == 35){
+    if( m == 35){
+      w ();
+      av();
+      if( e == 536){
+        av();
+        L(32);
+        *(int*) e=1;
+        *(int*)(e+4)=D;
+      }
+      while( m!=10){
+        L(m);
+        w ();
+      }
+      L(m);
+      L(2);
+    }
+    w ();
+  }
+  J=0;
+  e=m;
+  if( am ()){
+    L(32);
+    Z=D;
+    while( am ()){
+      L(m);
+      w ();
+    }
+    if( isdigit(e)){
+      C=strtol(Z,0,0);
+      e=2;
+    }
+    else{
+      *(char*) D=32;
+      e=strstr(Q,Z-1)-Q;
+      *(char*) D=0;
+      e=e*8+256;
+      if( e>536){
+        e=T+e;
+        if( *(int*) e == 1){
+          V=*(int*)(e+4);
+          al=m;
+          w ();
+          av();
+        }
+      }
+    }
+  }
+  else{
+    w ();
+    if( e == 39){
+      e=2;
+      an ();
+      C=m;
+      w ();
+      w ();
+    }
+    else if( e == 47&m == 42){
+      w ();
+      while( m){
+        while( m!=42)w ();
+        w ();
+        if( m == 47)m=0;
+      }
+      w ();
+      av();
+    }
+    else{
+      a="++#m--%am*@R<^1c/@%[_[H3c%@%[_[H3c+@.B#d-@%:_^BKd<<Z/03e>>`/03e<=0f>=/f<@.f>@1f==&g!='g&&k||#l&@.BCh^@.BSi|@.B+j~@/%Yd!@&d*@b";
 while( s=*(char*) a++){
 h=*(char*) a++;
 C=0;
@@ -112,6 +112,7 @@ break;
 }
 }
 }
+
 aw( d){
 while( d&&d!=-1){
 *(char*) v++=d;
