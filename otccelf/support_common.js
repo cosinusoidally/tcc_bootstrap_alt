c697546a1fb52576e5c1519caf24e4700aba874a
Liam Wilson <cosinusoidally@gmail.com>
2024-03-20 21:51:22 +0000

    start introducing virtual stack

--- otccelf/support_common.js
+++ otccelf/support_common.js
@@ -1,3 +1,5 @@
+var v_esp, v_ebp;
+
 function wi32(o, v) {
   wi8(o,v&0xFF);
   v=v>>8;
@@ -11,4 +13,28 @@ function wi32(o, v) {
 function ri32(o) {
   return (ri8(o)&255)       | (ri8(o+1)&255)<<8 |
          (ri8(o+2)&255)<<16 | (ri8(o+3)&255)<<24;
+}
+
+function v_alloca(x) {
+/* FIXME dummy impl */
+  return calloc(1,x);
+/*
+  v_esp=v_esp-x;
+  return esp;
+*/
+}
+
+function enter() {
+  puts("enter");
+  v_esp=v_esp-4;
+  wi32(v_esp,v_ebp);
+  v_ebp=v_esp;
+}
+
+function leave(x) {
+  puts("leave");
+  v_esp=v_ebp;
+  v_ebp=ri32(v_esp);
+  v_esp=v_esp+4;
+  return x;
 }
