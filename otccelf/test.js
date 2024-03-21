690fef6c38291d52fdd3700669f567793f6ed5b3
Liam Wilson <cosinusoidally@gmail.com>
2024-03-21 23:42:39 +0000

    more

--- otccelf/test.js
+++ otccelf/test.js
@@ -8,3 +8,8 @@ argc_argv=mk_argc_argv("otccelf ex1.c otccelf_js_js.exe");
 print(JSON.stringify(argc_argv));
  main(argc_argv.argc,argc_argv.argv);
 // main(0,0);
+
+e="f99cbf13a24ff16e1732756dea7ad36b3f073d628c8fd4bead71579cf7d543af";
+sha=root.sha256(vfs["otccelf_js_js.exe"]);
+print(sha);
+print(e==sha);
