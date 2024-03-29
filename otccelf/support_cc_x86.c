4de08affbd152d122911b1ae5dcbb0f01202ad6f
Liam Wilson <cosinusoidally@gmail.com>
2024-03-29 22:45:41 +0000

    more

--- otccelf/support_cc_x86.c
+++ otccelf/support_cc_x86.c
@@ -14,7 +14,6 @@ int v_alloca(int x);
 int enter(void);
 int leave(int x);
 
-int fgetc(int a);
 int isalnum(int a);
 int isspace(int a);
 int isdigit(int a);
@@ -22,9 +21,14 @@ int strtol(int a, int b, int c);
 int strstr(int a, int b);
 int memcpy(int a, int b, int c);
 int strcpy(int a, int b);
-int fopen(int a, int b);
 int fwrite(int a, int b, int c, int d);
+int puts(char *a);
+int printf(int a);
+
+/*
+int fgetc(int a);
+int fopen(int a, int b);
 int fclose(int a);
 int puts(int a);
-int printf(int a);
 int calloc(int a, int b);
+*/
