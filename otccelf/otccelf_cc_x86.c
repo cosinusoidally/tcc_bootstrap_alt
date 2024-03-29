a194fd1fc085b544556680b1955433f3dfa0c160
Liam Wilson <cosinusoidally@gmail.com>
2024-03-29 19:33:46 +0000

    change approach

--- otccelf/otccelf_cc_x86.c
+++ otccelf/otccelf_cc_x86.c
@@ -1,6 +1,4 @@
-int tok; int tokc; int tokl; int ch; int vars; int rsym; int prog; int ind;
-int loc; int glo; int file; int sym_stk; int dstk; int dptr; int dch;
-int last_id; int data; int text; int data_offset;
+int tok, tokc, tokl, ch, vars, rsym, prog, ind, loc, glo, file, sym_stk, dstk,dptr,dch,last_id, data, text, data_offset;
 
 int ALLOC_SIZE;
 
@@ -13,47 +11,50 @@ int TOK_IDENT, TOK_INT, TOK_IF, TOK_ELSE, TOK_WHILE, TOK_BREAK, TOK_RETURN;
 int TOK_FOR, TOK_DEFINE, TOK_MAIN, TOK_DUMMY, TOK_NUM, LOCAL, SYM_FORWARD;
 int SYM_DEFINE, TAG_TOK, TAG_MACRO;
 
-pdef(t){
-  *(char*) dstk++ = t;
+ pdef(t){
+  wi8(dstk, t);
+  dstk = dstk + 1;
 }
 
-inp (){
+ inp (){
   if(dptr){
-    ch=*(char*) dptr++;
+    ch = ri8(dptr);
+    dptr = dptr + 1;
     if( ch == TAG_MACRO){
       dptr=0;
       ch=dch;
     }
+  } else {
+    ch=fgetc(file);
   }
-    else ch=fgetc(file);
 }
 
-isid (){
-  return isalnum(ch) | ch == '_';
+ isid (){
+  return isalnum(ch) | ch == mk_char('_');
 }
 
-getq (){
-  if( ch == '\\'){
+ getq (){
+  if( ch == mk_char('\\')){
     inp ();
-    if( ch == 'n') {
-      ch = '\n';
+    if( ch == mk_char('n')) {
+      ch = mk_char('\n');
     }
   }
 }
 
-next(){
+ next(){
   int t, l, a;
-  while( isspace(ch) | ch == '#'){
-    if( ch == '#'){
+  while( isspace(ch) | ch == mk_char('#')){
+    if( ch == mk_char('#')){
       inp ();
       next();
       if( tok == TOK_DEFINE){
         next();
         pdef(TAG_TOK);
-        *(int*) tok=1;
-        *(int*)(tok+4)=dstk;
+        wi32(tok, 1);
+        wi32(tok+4, dstk);
       }
-      while( ch != '\n'){
+      while( ch != mk_char('\n')){
         pdef(ch);
         inp ();
       }
@@ -75,14 +76,14 @@ next(){
       tokc = strtol(last_id,0,0);
       tok = TOK_NUM;
     } else{
-      *(char*) dstk = TAG_TOK;
+      wi8(dstk, TAG_TOK);
       tok = strstr(sym_stk, last_id - 1) - sym_stk;
-      *(char*) dstk=0;
+      wi8(dstk, 0);
       tok=tok * 8 + TOK_IDENT;
       if( tok>TOK_DEFINE){
         tok=vars+tok;
-        if( *(int*) tok == SYM_DEFINE){
-          dptr=*(int*)(tok+4);
+        if( ri32(tok) == SYM_DEFINE){
+          dptr = ri32(tok+4);
           dch=ch;
           inp ();
           next();
@@ -91,34 +92,38 @@ next(){
     }
   } else{
     inp ();
-    if( tok == '\''){
+    if( tok == mk_char('\'')){
       tok = TOK_NUM;
       getq ();
       tokc=ch;
       inp ();
       inp ();
-    } else if( tok == '/' & ch == '*'){
+    } else if( tok == mk_char('/') & ch == mk_char('*')){
       inp ();
       while( ch){
-        while( ch != '*') {
+        while( ch != mk_char('*')) {
           inp ();
         }
         inp ();
-        if( ch == '/') {
+        if( ch == mk_char('/')) {
           ch=0;
         }
       }
       inp ();
       next();
     } else{
-      t="++#m--%am*@R<^1c/@%[_[H3c%@%[_[H3c+@.B#d-@%:_^BKd<<Z/03e>>`/03e<=0f>=/f<@.f>@1f==&g!='g&&k||#l&@.BCh^@.BSi|@.B+j~@/%Yd!@&d*@b";
-      while( l=*(char*) t++){
-        a=*(char*) t++;
+      t=mk_c_string("++#m--%am*@R<^1c/@%[_[H3c%@%[_[H3c+@.B#d-@%:_^BKd<<Z/03e>>`/03e<=0f>=/f<@.f>@1f==&g!='g&&k||#l&@.BCh^@.BSi|@.B+j~@/%Yd!@&d*@b");
+      while( l = ri8(t)){
+        t = t + 1;
+        a = ri8(t);
+        t = t + 1;
         tokc=0;
-        while((tokl = *(char*) t++ - 'b')<0) {
+        while((tokl = ri8(t) - mk_char('b'))<0) {
+          t = t + 1;
           tokc = tokc * 64 + tokl + 64;
         }
-        if( l == tok & (a == ch | a == '@')){
+        t = t + 1;
+        if( l == tok & (a == ch | a == mk_char('@'))){
           if( a == ch){
             inp ();
             tok = TOK_DUMMY;
@@ -126,35 +131,37 @@ next(){
           break;
         }
       }
+      t = t + 1;
     }
   }
 }
 
-o( n){
+ o( n){
   while( n && n!=-1){
-    *(char*) ind++ = n;
+    wi8(ind, n);
+    ind = ind + 1;
     n = n >> 8;
   }
 }
 
-put32(t, n){
-  *(char*) t++ = n;
-  *(char*) t++ = n>>8;
-  *(char*) t++ = n>>16;
-  *(char*) t++ = n>>24;
+ put32(t, n){
+  wi8(t, n);     t = t + 1;
+  wi8(t,  n>>8); t = t + 1;
+  wi8(t, n>>16); t = t + 1;
+  wi8(t, n>>24); t = t + 1;
 }
 
-get32(t){
+ get32(t){
   int n;
-  return (*(char*) t&255)        | (*(char*)(t+1)&255)<<8 |
-         (*(char*)(t+2)&255)<<16 | (*(char*)(t+3)&255)<<24;
+  return (ri8(t)&255)       | (ri8(t+1)&255)<<8 |
+         (ri8(t+2)&255)<<16 | (ri8(t+3)&255)<<24;
 }
 
-gsym1(t, b){
+ gsym1(t, b){
   int d;
   while( t){
     d=get32(t);
-    if( *(char*)(t-1) == 5){
+    if( ri8(t-1) == 5){
       if( b >= data && b < glo)
         put32(t, b + data_offset);
       else
@@ -167,11 +174,11 @@ gsym1(t, b){
   }
 }
 
-gsym(t){
+ gsym(t){
   gsym1(t, ind);
 }
 
-oad(n, t){
+ oad(n, t){
   o( n);
   put32(ind, t);
   t = ind;
@@ -179,20 +186,20 @@ oad(n, t){
   return t;
 }
 
-li(t){
+ li(t){
   oad(184, t);
 }
 
-gjmp(t){
+ gjmp(t){
   return oad(233, t);
 }
 
-gtst(l, t){
+ gtst(l, t){
   o( 1032325);
   return oad(132+l, t);
 }
 
-gcmp(t){
+ gcmp(t){
   o( 49465);
   li(0);
   o( 15);
@@ -200,29 +207,30 @@ gcmp(t){
   o( 192);
 }
 
-gmov(l, t){
+ gmov(l, t){
   int d;
   o( l+131);
-  d = *(int*) t;
+  d = ri32(t);
   if( d && d < LOCAL) {
     oad(133,d);
   } else {
     t = t + 4;
-    *(int*) t = oad(5,*(int*) t);
+    wi32(t, oad(5,ri32(t)));
   }
 }
 
-unary(l){
+ unary(l){
   int n, t, a, c;
   n=1;
-  if( tok == '\"'){
+  if( tok == mk_char('\"')){
     li(glo+data_offset);
-    while( ch!='\"'){
+    while( ch!=mk_char('\"')){
       getq ();
-      *(char*) glo++=ch;
+      wi8(glo, ch);
+      glo = glo + 1;
       inp ();
     }
-    *(char*) glo=0;
+    wi8(glo, 0);
     glo=glo+4&-4;
     inp ();
     next();
@@ -237,22 +245,22 @@ unary(l){
     else if( c == 2){
       unary(0);
       oad(185,0);
-      if( t == '!') {
+      if( t == mk_char('!')) {
         gcmp(a);
       } else {
         o( a);
       }
     }
-    else if( t == '('){
+    else if( t == mk_char('(')){
       expr ();
       next();
     }
-    else if( t == '*'){
+    else if( t == mk_char('*')){
       next();
       t=tok;
       next();
       next();
-      if( tok == '*'){
+      if( tok == mk_char('*')){
         next();
         next();
         next();
@@ -261,7 +269,7 @@ unary(l){
       }
       next();
       unary(0);
-      if( tok == '='){
+      if( tok == mk_char('=')){
         next();
         o( 80);
         expr ();
@@ -275,17 +283,17 @@ unary(l){
         }
         ind++;
       }
-    } else if( t == '&'){
+    } else if( t == mk_char('&')){
       gmov(10,tok);
       next();
     }
     else{
       n=0;
-      if( tok == '=' & l){
+      if( tok == mk_char('=') & l){
         next();
         expr ();
         gmov(6,t);
-      } else if( tok != '('){
+      } else if( tok != mk_char('(')){
         gmov(8,t);
         if( tokl == 11){
           gmov(0,t);
@@ -295,15 +303,15 @@ unary(l){
       }
     }
   }
-  if( tok == '('){
+  if( tok == mk_char('(')){
     if( n)o( 80);
     a=oad(60545,0);
     next();
     l=0;
-    while( tok != ')'){
+    while( tok != mk_char(')')){
       expr ();
       oad(2393225,l);
-      if( tok == ',') {
+      if( tok == mk_char(',')) {
         next();
       }
       l=l+4;
@@ -316,13 +324,13 @@ unary(l){
     }
     else{
       t=t+4;
-      *(int*) t=oad(232,*(int*) t);
+      wi32(t, oad(232,ri32(t)));
     }
     if( l) oad(50305,l);
   }
 }
 
-sum(l){
+ sum(l){
   int t, n, a;
   if( l--== 1) unary(1);
   else{
@@ -345,7 +353,7 @@ sum(l){
         }
         else{
           o( t);
-          if( n == '%') {
+          if( n == mk_char('%')) {
             o( 146);
           }
         }
@@ -361,32 +369,34 @@ sum(l){
   }
 }
 
-expr (){
+ expr (){
   sum(11);
 }
 
-test_expr (){
+ test_expr (){
   expr ();
   return gtst(0,0);
 }
 
-block(l){
+ block(l){
   int a , n, t;
+  enter();
+  a = v_alloca(4);
   if( tok == TOK_IF){
     next();
     next();
-    a=test_expr ();
+    wi32(a, test_expr());
     next();
     block(l);
     if( tok == TOK_ELSE){
       next();
       n=gjmp(0);
-      gsym(a);
+      gsym(ri32(a));
       block(l);
       gsym(n);
     }
     else{
-      gsym(a);
+      gsym(ri32(a));
     }
   }
   else if( tok == TOK_WHILE | tok == TOK_FOR){
@@ -395,20 +405,20 @@ block(l){
     next();
     if( t == TOK_WHILE){
       n=ind;
-      a=test_expr ();
+      wi32(a, test_expr());
     }
     else{
-      if( tok != ';') {
+      if( tok != mk_char(';')) {
         expr ();
       }
       next();
       n=ind;
-      a=0;
-      if( tok != ';') {
-        a=test_expr ();
+      wi32(a, 0);
+      if( tok != mk_char(';')) {
+        wi32(a, test_expr());
       }
       next();
-      if( tok != ')'){
+      if( tok != mk_char(')')){
         t=gjmp(0);
         expr ();
         gjmp(n-ind-5);
@@ -417,14 +427,14 @@ block(l){
       }
     }
     next();
-    block(&a);
+    block(a);
     gjmp(n-ind-5);
-    gsym(a);
+    gsym(ri32(a));
   }
-  else if( tok == '{'){
+  else if( tok == mk_char('{')){
     next();
     decl(1);
-    while( tok != '}') {
+    while( tok != mk_char('}')) {
       block(l);
     }
     next();
@@ -432,51 +442,52 @@ block(l){
   else{
     if( tok == TOK_RETURN){
       next();
-      if( tok != ';') {
+      if( tok != mk_char(';')) {
         expr ();
       }
       rsym=gjmp(rsym);
     } else if( tok == TOK_BREAK){
       next();
-      *(int*) l=gjmp(*(int*) l);
-    } else if( tok != ';') {
+      wi32(l, gjmp(ri32(l)));
+    } else if( tok != mk_char(';')) {
       expr ();
     }
     next();
   }
+  leave(0);
 }
 
-decl(l){
+ decl(l){
   int a;
   while( tok == TOK_INT | tok != -1 & !l){
     if( tok == TOK_INT){
       next();
-      while( tok != ';'){
+      while( tok != mk_char(';')){
         if( l){
           loc=loc+4;
-          *(int*) tok=-loc;
+          wi32(tok, -loc);
         }
         else{
-          *(int*) tok=glo;
+          wi32(tok, glo);
           glo=glo+4;
         }
         next();
-        if( tok == ',') {
+        if( tok == mk_char(',')) {
           next();
         }
       }
       next();
     }
     else{
-      *(int*) tok=ind;
+      wi32(tok, ind);
       next();
       next();
       a=8;
-      while( tok != ')'){
-        *(int*) tok=a;
+      while( tok != mk_char(')')){
+        wi32(tok, a);
         a=a+4;
         next();
-        if( tok == ',') {
+        if( tok == mk_char(',')) {
           next();
         }
       }
@@ -492,12 +503,12 @@ decl(l){
   }
 }
 
-gle32( n){
+ gle32( n){
   put32(glo,n);
   glo=glo+4;
 }
 
-gphdr1(n,t){
+ gphdr1(n,t){
   gle32( n);
   n = n + ELF_BASE;
   gle32( n);
@@ -506,20 +517,20 @@ gphdr1(n,t){
   gle32( t);
 }
 
-elf_reloc(l){
+ elf_reloc(l){
   int t,a,n,p,b,c;
   p=0;
   t=sym_stk;
   while( 1){
     t++;
     a=t;
-    while( *(char*) t != TAG_TOK && t < dstk) {
+    while( ri8(t) != TAG_TOK && t < dstk) {
       t++;
     }
     if( t == dstk)break;
     tok = vars + (a - sym_stk) * 8 + TOK_IDENT - 8;
-    b=*(int*) tok;
-    n=*(int*)(tok+4);
+    b = ri32(tok);
+    n = ri32(tok+4);
     if( n&&b!=1){
       if(!b){
         if(!l){
@@ -535,7 +546,7 @@ elf_reloc(l){
           p++;
           while( n){
             a=get32(n);
-            c=*(char*)(n-1)!=5;
+            c = ri8(n-1)!=5;
             put32(n,-c*4);
             gle32( n-prog+text+data_offset);
             gle32( p*256+c+1);
@@ -549,13 +560,13 @@ elf_reloc(l){
   }
 }
 
-elf_out(c){
+ elf_out(c){
   int glo_saved, dynstr, dynstr_size, dynsym, hash, rel, n, t, text_size;
   text=glo;
   text_size=ind-prog;
   ind=prog;
   o( 5264472);
-  t = *(int*)(vars + TOK_MAIN);
+  t = ri32(vars + TOK_MAIN);
   oad(232,t-ind-5);
   o( 50057);
   li(1);
@@ -563,8 +574,8 @@ elf_out(c){
   glo=glo+text_size;
   dynstr=glo;
   glo++;
-  glo=strcpy(glo,"libc.so.6")+10;
-  glo=strcpy(glo,"libdl.so.2")+11;
+  glo=strcpy(glo,mk_c_string("libc.so.6"))+10;
+  glo=strcpy(glo,mk_c_string("libdl.so.2"))+11;
   elf_reloc(0);
   dynstr_size=glo-dynstr;
   glo=(glo+3)&-4;
@@ -614,7 +625,7 @@ elf_out(c){
   gphdr1(DYNAMIC_OFFSET, DYNAMIC_SIZE);
   gle32( 6);
   gle32( 4);
-  glo=strcpy(glo,"/lib/ld-linux.so.2")+20;
+  glo=strcpy(glo,mk_c_string("/lib/ld-linux.so.2"))+20;
   gle32( 1);
   gle32( 1);
   gle32( 1);
@@ -637,12 +648,12 @@ elf_out(c){
   gle32( 8);
   gle32( 0);
   gle32( 0);
-  t=fopen(c,"w");
+  t=fopen(c,mk_c_string("wb"));
   fwrite(data,1,glo_saved-data,t);
   fclose(t);
 }
 
-init_globals(){
+ init_globals(){
   ALLOC_SIZE = 99999;
 
   ELF_BASE = 0x08048000;
@@ -679,25 +690,27 @@ init_globals(){
   SYM_FORWARD = 0;
   SYM_DEFINE  = 1;
 
-  TAG_TOK   =  ' ';
+  TAG_TOK   =  mk_char(' ');
   TAG_MACRO =  2;
 }
 
 
-main(n,t){
+ main(n,t){
+  puts("otccelf start");
+  init_c();
   init_globals();
   if( n<3){
     printf("usage: otccelf file.c outfile\n");
     return 0;
   }
   dstk=strcpy(sym_stk = calloc(1, ALLOC_SIZE),
-              " int if else while break return for define main ") + TOK_STR_SIZE;
+       mk_c_string(" int if else while break return for define main ")) + TOK_STR_SIZE;
   glo = data = calloc(1, ALLOC_SIZE);
   ind = prog = calloc(1, ALLOC_SIZE);
   vars = calloc(1, ALLOC_SIZE);
 
   t = t + 4;
-  file=fopen(*(int*)t, "r");
+  file=fopen(ri32(t), mk_c_string("r"));
 
   data_offset = ELF_BASE - data;
   glo = glo + ELFSTART_SIZE;
@@ -707,6 +720,9 @@ main(n,t){
   next();
   decl(0);
   t = t + 4;
-  elf_out(*(int*)t);
+  elf_out(ri32(t));
+  puts("otccelf complete");
+
+
   return 0;
 }
