6304542e4bf7c9bce47b38373b21925328d2d9d0
Liam Wilson <cosinusoidally@gmail.com>
2024-04-11 17:57:48 +0100

    shr impl

--- m2min/otccelf_cc_x86_min.c
+++ m2min/otccelf_cc_x86_min.c
@@ -112,6 +112,22 @@ int shl(int a, int b){
 	);
 }
 
+int shr(int a, int b){
+/*	return a >> b; */
+	asm(
+		"lea_eax,[ebp+DWORD] %-4"
+		"mov_eax,[eax]"
+		"push_eax"
+		"lea_eax,[ebp+DWORD] %-8"
+		"mov_eax,[eax]"
+		"pop_ebx"
+		"mov_ecx,eax"
+		"mov_eax,ebx"
+		"sar_eax,cl"
+		"ret"
+	);
+}
+
 int ri8(int o) {
 /*
   char *h = 0;
