573a97e34d1d034eae6b929dbcea6749db9645d7
Liam Wilson <cosinusoidally@gmail.com>
2024-04-11 18:45:04 +0100

    gte impl

--- m2min/M2_tcc_min.c
+++ m2min/M2_tcc_min.c
@@ -977,8 +977,8 @@ void arithmetic_recursion(FUNCTION f, char* s1, char* s2, char* name, FUNCTION i
 	if(match(name, global_token->s))
 	{
 /* LJW debug logging */
-		if(match(name, "<=")){
-			fputs("lte on line: ", stdout);
+		if(match(name, ">=")){
+			fputs("gte on line: ", stdout);
 			fputs(int2str(global_token->linenumber,10,0), stdout);
 			fputs("\n", stdout);
 		}
--- m2min/otccelf_cc_x86_min.c
+++ m2min/otccelf_cc_x86_min.c
@@ -176,6 +176,22 @@ int lte(int a, int b){
 	);
 }
 
+int gte(int a, int b){
+/*	return a >= b; */
+	asm(
+		"lea_eax,[ebp+DWORD] %-4"
+		"mov_eax,[eax]"
+		"push_eax"
+		"lea_eax,[ebp+DWORD] %-8"
+		"mov_eax,[eax]"
+		"pop_ebx"
+		"cmp"
+		"setge_al"
+		"movzx_eax,al"
+		"ret"
+	);
+}
+
 int ri8(int o) {
 /*
   char *h = 0;
