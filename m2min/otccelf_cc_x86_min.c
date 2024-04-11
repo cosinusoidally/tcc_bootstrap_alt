07d90e25337565caec699a1a05cc6ba28d49288d
Liam Wilson <cosinusoidally@gmail.com>
2024-04-11 20:39:09 +0100

    and impl

--- m2min/otccelf_cc_x86_min.c
+++ m2min/otccelf_cc_x86_min.c
@@ -192,6 +192,20 @@ int gte(int a, int b){
 	);
 }
 
+int and(int a, int b){
+/*	return a & b; */
+	asm(
+		"lea_eax,[ebp+DWORD] %-4"
+		"mov_eax,[eax]"
+		"push_eax"
+		"lea_eax,[ebp+DWORD] %-8"
+		"mov_eax,[eax]"
+		"pop_ebx"
+		"and_eax,ebx"
+		"ret"
+	);
+}
+
 int ri8(int o) {
 /*
   char *h = 0;
