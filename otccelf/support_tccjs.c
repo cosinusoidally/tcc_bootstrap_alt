b3d75721a8d4ea6c2180173cc3e308bac4518559
Liam Wilson <cosinusoidally@gmail.com>
2024-04-16 23:19:28 +0100

    more

--- otccelf/support_tccjs.c
+++ otccelf/support_tccjs.c
@@ -2,3 +2,23 @@ v_malloc(){
   puts("v_malloc not impl");
   exit(1);
 }
+
+v_strlen(){
+  puts("v_strlen not impl");
+  exit(1);
+}
+
+v_strcpy(){
+  puts("v_strcpy not impl");
+  exit(1);
+}
+
+wi8(){
+  puts("wi8 not impl");
+  exit(1);
+}
+
+mk_c_string(){
+  puts("mk_c_string not impl");
+  exit(1);
+}
