31b2bc10f1c8f356b3fc0721b8ed7340db0ef547
Liam Wilson <cosinusoidally@gmail.com>
2024-04-16 23:22:48 +0100

    more

--- otccelf/support_tccjs.c
+++ otccelf/support_tccjs.c
@@ -18,7 +18,55 @@ wi8(){
   exit(1);
 }
 
+wi32(){
+  puts("wi32 not impl");
+  exit(1);
+}
+
+ri8(){
+  puts("ri8 not impl");
+  exit(1);
+}
+
+ri32(){
+  puts("ri32 not impl");
+  exit(1);
+}
+
 mk_c_string(){
   puts("mk_c_string not impl");
   exit(1);
 }
+
+urs(){
+  puts("urs not impl");
+  exit(1);
+}
+
+int NULL;
+
+v_free(){
+  puts("v_free not impl");
+  exit(1);
+}
+
+v_alloca(){
+  puts("v_alloca not impl");
+  exit(1);
+}
+
+mk_char(){
+  puts("v_alloca not impl");
+  exit(1);
+}
+
+expect(){
+  puts("expect not impl");
+  exit(1);
+}
+
+enter(){
+  puts("enter not impl");
+  exit(1);
+}
+
