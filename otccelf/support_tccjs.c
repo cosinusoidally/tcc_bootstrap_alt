8ef7b7a2f3400d2c1a51423d831b868fa3b73193
Liam Wilson <cosinusoidally@gmail.com>
2024-04-16 23:52:15 +0100

    more

--- otccelf/support_tccjs.c
+++ otccelf/support_tccjs.c
@@ -10,24 +10,28 @@ int v_stack;
 int v_esp;
 int v_ebp;
 
-wi8(){
-  puts("wi8 not impl");
-  exit(1);
+wi8(o, v){
+  *(char*)o = (v & 255);
 }
 
-wi32(){
-  puts("wi32 not impl");
-  exit(1);
+ri8(o){
+ return *(char*)o;
 }
 
-ri8(){
-  puts("ri8 not impl");
-  exit(1);
+
+wi32(o, v) {
+  wi8(o,v&0xFF);
+  v=v>>8;
+  wi8(o+1,v&0xFF);
+  v=v>>8;
+  wi8(o+2,v&0xFF);
+  v=v>>8;
+  wi8(o+3,v&0xFF);
 }
 
-ri32(){
-  puts("ri32 not impl");
-  exit(1);
+ri32(o) {
+  return (ri8(o)&255)       | (ri8(o+1)&255)<<8 |
+         (ri8(o+2)&255)<<16 | (ri8(o+3)&255)<<24;
 }
 
 urs(){
