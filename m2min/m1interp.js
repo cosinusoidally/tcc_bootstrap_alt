faee265528ca73e82dc90baf5ae8ce87886cd6a0
Liam Wilson <cosinusoidally@gmail.com>
2024-05-18 21:27:41 +0100

    more

--- m2min/m1interp.js
+++ m2min/m1interp.js
@@ -1,1 +1,16 @@
 print("hello world");
+a=read("artifacts/M2_simple_asm-0.hex2");
+
+a=a.split("\n");
+
+labels={};
+
+for(var i=0;i<a.length;i++){
+  var l;
+  l=a[i];
+  if(l[0]===":"){
+    labels[l.split(":")[1]] = i;
+  } else {
+
+  }
+}
