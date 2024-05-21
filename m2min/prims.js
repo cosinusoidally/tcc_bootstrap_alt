7d2c96eb8f2aededb108aaadc26c70ac8a8c1013
Liam Wilson <cosinusoidally@gmail.com>
2024-05-21 19:26:54 +0100

    more

--- m2min/prims.js
+++ m2min/prims.js
@@ -32,3 +32,25 @@ prim_ops["gt"]=function(){
   op_ret();
 }
 
+prim_ops["fgetc"]=function(){
+  var a = get_arg(0);
+
+  print("fgetc(" +a+")");
+
+  if(a === in_file_num) {
+    if(in_file[1] < in_file[0].length) {
+      eax = in_file[0][in_file[1]];
+      print("fgetc: "+String.fromCharCode(eax));
+      in_file[1]=in_file[1]+1;
+    } else {
+      print("fgetc: EOF");
+      eax = -1;
+    }
+  } else {
+    print("fgetc wrong file descriptor");
+    throw "fgetc";
+  }
+
+  op_ret();
+}
+
