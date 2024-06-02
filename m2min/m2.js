4b0706ced0281fc606fa6d679452cd290e840397
Liam Wilson <cosinusoidally@gmail.com>
2024-06-02 03:49:29 +0100

    more

--- m2min/m2.js
+++ m2min/m2.js
@@ -2,100 +2,110 @@ load("simple_support_js_m2.js");
 load("M2_simple_asm.js");
 
 function add(a, b){
-/*	return a + b; */
-  err();
+  a = a | 0;
+  b = b | 0;
+  return a + b;
 }
 
 function sub(a, b){
   a = a | 0;
   b = b | 0;
-/*	return a - b; */
-  err();
+  return a - b;
 }
 
 function mul(a, b){
-/*	return a * b; */
-  err();
+  a = a | 0;
+  b = b | 0;
+  return a * b;
 }
 
 function shl(a, b){
+  a = a | 0;
+  b = b | 0;
 /*	return a << b; */
   err();
 }
 
 function shr(a, b){
+  a = a | 0;
+  b = b | 0;
 /*	return a >> b; */
   err();
 }
 
 function lt(a, b){
-/*	return a < b; */
-  err();
+  a = a | 0;
+  b = b | 0;
+  return (a < b) | 0;
 }
 
 function gt(a, b){
+  a = a | 0;
+  b = b | 0;
 /*	return a > b; */
   err();
 }
 
 function lte(a, b){
+  a = a | 0;
+  b = b | 0;
 /*	return a <= b; */
   err();
 }
 
 function gte(a, b){
+  a = a | 0;
+  b = b | 0;
 /*	return a >= b; */
   err();
 }
 
 function and(a, b){
+  a = a | 0;
+  b = b | 0;
 /*	return a & b; */
   err();
 }
 
 function or(a, b){
+  a = a | 0;
+  b = b | 0;
 /*	return a | b; */
   err();
 }
 
-function ri8(o) {
-/*
-  char *h = 0;
-  return h[o] & 0xFF;
-*/
-  err();
-}
-
 function eq(a, b){
+  a = a | 0;
+  b = b | 0;
 /*	return a == b; */
+  return (a == b) | 0;
   err();
 }
 
 function neq(a, b){
+  a = a | 0;
+  b = b | 0;
 /*	return a != b; */
   err();
 }
 
 function xor(a, b){
+  a = a | 0;
+  b = b | 0;
 /*	return a ^ b; */
   err();
 }
 
-function wi8(o,v) {
-/*
-  char *h = 0;
-  h[o]=v;
-  return;
-*/
-  err();
-}
-
 function div(a, b){
+  a = a | 0;
+  b = b | 0;
 /*	return a / b; */
   err();
 }
 
 function mod(a, b){
+  a = a | 0;
+  b = b | 0;
 /*	return a % b; */
   err();
 }
@@ -121,5 +131,23 @@ function brk(addr) {
 
 function exit(value) {
 }
+
+function ri8(o) {
+/*
+  char *h = 0;
+  return h[o] & 0xFF;
+*/
+  err();
+}
+
+function wi8(o,v) {
+/*
+  char *h = 0;
+  h[o]=v;
+  return;
+*/
+  err();
+}
+
 
 main();
