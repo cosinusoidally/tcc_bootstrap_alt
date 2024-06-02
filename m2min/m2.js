598fe9cd4ce50e437676c36e434b5de2e73216a1
Liam Wilson <cosinusoidally@gmail.com>
2024-06-02 03:44:52 +0100

    more

--- m2min/m2.js
+++ m2min/m2.js
@@ -1,2 +1,125 @@
 load("simple_support_js_m2.js");
 load("M2_simple_asm.js");
+
+function add(a, b){
+/*	return a + b; */
+  err();
+}
+
+function sub(a, b){
+  a = a | 0;
+  b = b | 0;
+/*	return a - b; */
+  err();
+}
+
+function mul(a, b){
+/*	return a * b; */
+  err();
+}
+
+function shl(a, b){
+/*	return a << b; */
+  err();
+}
+
+function shr(a, b){
+/*	return a >> b; */
+  err();
+}
+
+function lt(a, b){
+/*	return a < b; */
+  err();
+}
+
+function gt(a, b){
+/*	return a > b; */
+  err();
+}
+
+function lte(a, b){
+/*	return a <= b; */
+  err();
+}
+
+function gte(a, b){
+/*	return a >= b; */
+  err();
+}
+
+function and(a, b){
+/*	return a & b; */
+  err();
+}
+
+function or(a, b){
+/*	return a | b; */
+  err();
+}
+
+function ri8(o) {
+/*
+  char *h = 0;
+  return h[o] & 0xFF;
+*/
+  err();
+}
+
+function eq(a, b){
+/*	return a == b; */
+  err();
+}
+
+function neq(a, b){
+/*	return a != b; */
+  err();
+}
+
+function xor(a, b){
+/*	return a ^ b; */
+  err();
+}
+
+function wi8(o,v) {
+/*
+  char *h = 0;
+  h[o]=v;
+  return;
+*/
+  err();
+}
+
+function div(a, b){
+/*	return a / b; */
+  err();
+}
+
+function mod(a, b){
+/*	return a % b; */
+  err();
+}
+
+function fgetc(f) {
+  err();
+}
+
+function fputc(s, f) {
+  err();
+}
+
+function open(name, flag, mode) {
+  err();
+}
+
+function close(fd) {
+  err();
+}
+
+function brk(addr) {
+}
+
+function exit(value) {
+}
+
+main();
