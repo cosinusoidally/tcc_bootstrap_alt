e7f2930d9590ce793568624e02a49c803d74eb85
Liam Wilson <cosinusoidally@gmail.com>
2024-10-09 22:19:28 +0100

    more

--- m2min/M2_nano_test.js
+++ m2min/M2_nano_test.js
@@ -1,2 +1,20 @@
+var acc;
+
+var a0;
+var a1;
+
+function f3(){
+  acc='a';
+}
+
+function bar(){
+  acc="Hello world";
+}
+
+function foo(){
+  add();
+}
+
 function main(){
+  a0=20;a1=21;foo();
 }
