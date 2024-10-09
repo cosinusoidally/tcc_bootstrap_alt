c390467a5ea0c2fc3ed60e70f4f54898b93ff46e
Liam Wilson <cosinusoidally@gmail.com>
2024-10-09 22:24:07 +0100

    more

--- m2min/M2_nano_test.js
+++ m2min/M2_nano_test.js
@@ -3,6 +3,21 @@ var acc;
 var a0;
 var a1;
 
+function add(){
+/* this is not currently correct, just here to test out asm */
+/* what it should do is acc = a0 + a1; */
+	asm(
+		"lea_eax,[ebp+DWORD] %-4"
+		"mov_eax,[eax]"
+		"push_eax"
+		"lea_eax,[ebp+DWORD] %-8"
+		"mov_eax,[eax]"
+		"pop_ebx"
+		"add_eax,ebx"
+		"ret"
+		);
+}
+
 function f3(){
   acc='a';
 }
