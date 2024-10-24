3ca9742bc21f12f3f2f9169f00d1ddcff9596222
Liam Wilson <cosinusoidally@gmail.com>
2024-10-24 17:35:51 +0100

    more

---
+++ nanoc/nanoc_test1.c
@@ -0,0 +1,21 @@
+int acc;
+int a1;
+int ip;
+
+int next_block_;
+
+int add();
+int end();
+
+/* these are blocks */
+int test(){
+  acc = 123;
+  a1 = 1000;
+  add();
+  ip = next_block_; /* this would call the next block */
+}
+
+int next_block(){
+  acc = 0;
+  end();
+}
