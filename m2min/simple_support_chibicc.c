f836399de2fc91b6e96cb0d5e7b12955f13b0283
Liam Wilson <cosinusoidally@gmail.com>
2025-02-26 20:31:56 +0000

    more

--- m2min/simple_support_chibicc.c
+++ m2min/simple_support_chibicc.c
@@ -3,6 +3,22 @@ int and(int a, int b);
 int ri8(int a);
 int shl(int a, int b);
 int add(int a, int b);
-int wi8(int a, int B);
-int shr(int a, int B);
-int sub(int a, int B);
+int wi8(int a, int b);
+int shr(int a, int b);
+int sub(int a, int b);
+int lt(int a, int b);
+int gt(int a, int b);
+int mks(int a);
+int mul(int a, int b);
+int eq(int a, int b);
+int neq(int a, int b);
+int mod(int a, int b);
+int _div(int a, int b);
+int gte(int a, int b);
+int mkc(int a);
+
+int v_calloc(int a, int b);
+int v_fgetc(int a);
+int v_free(int a);
+
+int emit_out(int a);
