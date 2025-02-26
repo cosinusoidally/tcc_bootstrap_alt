eed42c6175c70ce703b39fd93e0da5bf65922670
Liam Wilson <cosinusoidally@gmail.com>
2025-02-26 20:28:04 +0000

    start adding chibicc support

---
+++ m2min/simple_support_chibicc.c
@@ -0,0 +1,8 @@
+int or(int a, int b);
+int and(int a, int b);
+int ri8(int a);
+int shl(int a, int b);
+int add(int a, int b);
+int wi8(int a, int B);
+int shr(int a, int B);
+int sub(int a, int B);
